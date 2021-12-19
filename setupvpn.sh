5231f52dcc90ce5d6589ea5251fe0132a36f6593
Konstantin Lyschenko <klyschenko@ourgapps.com>
2021-12-19 18:03:25 +0300

    Added changed script and config file

---
+++ setupvpn.sh
@@ -0,0 +1,13 @@
+
+cat <<EOF> setupvpn.sh
+#!/bin/bash
+echo "deb https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" > /etc/apt/sources.list.d/mongodb-org-4.4.list
+echo "deb http://repo.pritunl.com/stable/apt xenial main" > /etc/apt/sources.list.d/pritunl.list
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 0C49F3730359A14518585931BC711F9BA15703C6
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+apt-get --assume-yes update
+apt-get --assume-yes upgrade
+apt-get --assume-yes install pritunl mongodb-org
+systemctl start pritunl mongod
+systemctl enable pritunl mongod
+EOF
