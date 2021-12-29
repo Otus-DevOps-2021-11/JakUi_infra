0e05c28f8b09f49c1f7a8d7fecd17c72c41175d5
JakUi <kawtan34@gmail.com>
2021-12-29 12:24:37 +0300

    Create file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 provider "yandex" {
-  token     = "<OAuth или статический ключ сервисного аккаунта>"
-  cloud_id  = "<идентификатор облака>"
-  folder_id = "<идентификатор каталога>"
+  token     = "AQAAAABbbNgTAATuwaWzacySDkZ4g99PYL4OuAo"
+  cloud_id  = "b1gjitf868lee0aep2g3"
+  folder_id = "b1gsa3n8df9fiuhlhk24"
   zone      = "ru-central1-a"
 }
