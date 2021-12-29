bcfe65420565e2922f344450830926f633a3112c
JakUi <kawtan34@gmail.com>
2021-12-29 18:50:55 +0300

    Removed unused code

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,3 @@
-# provider "yandex" {
-#   token     = "AQAAAABbbNgTAATuwaWzacySDkZ4g99PYL4OuAo"
-#   #  service_account_key_file = "path_to_service_account_key_file"  !!! Вставить путь к файлу, убрать токен
-#   cloud_id  = "b1gjitf868lee0aep2g3"
-#   folder_id = "b1gsa3n8df9fiuhlhk24"
-#   zone      = "ru-central1-b"
-# }
 
 provider "yandex" {
   # service_account_key_file = var.service_account_key_file
@@ -22,34 +15,17 @@ resource "yandex_compute_instance" "app" {
     memory = 2
   }
 
-  # boot_disk {
-  #   initialize_params {
-  #     image_id = "fd87rrd7ekbn2j6r4d14"
-  #   }
-  # }
-
   boot_disk {
     initialize_params {
-      # Указать id образа созданного в предыдущем домашем задании
       image_id = var.image_id
     }
   }
-
-  # network_interface {
-  #   # Указан id подсети default-ru-central1-a
-  #   subnet_id = "e2lerbr3up19r3j6nqd1"
-  #   nat       = true
-  # }
 
   network_interface {
     # Указан id подсети default-ru-central1-a
     subnet_id = var.subnet_id
     nat       = true
   }
-
-  #   metadata = {
-  #   ssh-keys = "ubuntu:${file("~/.ssh/ubuntu.pub")}"
-  # }
 
   metadata = {
     ssh-keys = "ubuntu:${file(var.public_key_path)}"
@@ -61,9 +37,7 @@ resource "yandex_compute_instance" "app" {
     user  = "ubuntu"
     agent = false
     # путь до приватного ключа
-    # private_key = file("~/.ssh/ubuntu")
-    # private_key = var.private_key_path
-    private_key = file(var.private_key_path)
+    private_key   = file(var.private_key_path)
   }
 
   provisioner "file" {
