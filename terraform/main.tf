ecd0b310e1e4e2236291c59de37575170f443b9c
JakUi <kawtan34@gmail.com>
2021-12-29 11:05:10 +0300

    Added provider info

---
+++ terraform/main.tf
@@ -0,0 +1,6 @@
+provider "yandex" {
+  token     = "<OAuth или статический ключ сервисного аккаунта>"
+  cloud_id  = "<идентификатор облака>"
+  folder_id = "<идентификатор каталога>"
+  zone      = "ru-central1-a"
+}
