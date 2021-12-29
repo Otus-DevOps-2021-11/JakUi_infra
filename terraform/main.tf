f9632a0297e29c9c783cdb94958dd8d129453289
JakUi <kawtan34@gmail.com>
2021-12-29 13:15:03 +0300

    Did homework until 19 page

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,28 @@
 provider "yandex" {
   token     = "AQAAAABbbNgTAATuwaWzacySDkZ4g99PYL4OuAo"
+  #  service_account_key_file = "path_to_service_account_key_file"  !!! Вставить путь к файлу, убрать токен
   cloud_id  = "b1gjitf868lee0aep2g3"
   folder_id = "b1gsa3n8df9fiuhlhk24"
-  zone      = "ru-central1-a"
+  zone      = "ru-central1-b"
+}
+
+resource "yandex_compute_instance" "app" {
+  name = "reddit-app"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd87rrd7ekbn2j6r4d14"
+    }
+  }
+
+  network_interface {
+    # Указан id подсети default-ru-central1-a
+    subnet_id = "e2lerbr3up19r3j6nqd1"
+    nat       = true
+  }
 }
