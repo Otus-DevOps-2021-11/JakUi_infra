30e377bb2394ef1019bb6df15a54f302f710a65d
JakUi <kawtan34@gmail.com>
2021-12-29 17:49:17 +0300

    Added deploy

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,4 +29,23 @@ resource "yandex_compute_instance" "app" {
   metadata = {
   ssh-keys = "ubuntu:${file("~/.ssh/ubuntu.pub")}"
 }
+
+  connection {
+  type = "ssh"
+  host = yandex_compute_instance.app.network_interface.0.nat_ip_address
+  user = "ubuntu"
+  agent = false
+  # путь до приватного ключа
+  private_key = file("~/.ssh/ubuntu")
+  }
+  
+  provisioner "file" {
+  source = "files/puma.service"
+  destination = "/tmp/puma.service"
+}
+
+  provisioner "remote-exec" {
+  script = "files/deploy.sh"
+}
+
 }
