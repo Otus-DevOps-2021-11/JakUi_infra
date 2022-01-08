6fc8ad462ab0f23f61e7dc34bbef95ecd468105f
JakUi <kawtan34@gmail.com>
2022-01-08 16:06:30 +0300

    Fixed

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,5 +2,6 @@ output "external_ip_address_app" {
   value = yandex_compute_instance.app.network_interface.0.nat_ip_address
 }
 output "external_load_balancer_address_app" {
-  value = yandex_lb_network_load_balancer.reddit_app_load_balancer.listener.*.external_address_spec[*][*]["address"]
+  value = element(yandex_lb_network_load_balancer.reddit_app_load_balancer.listener.*.external_address_spec[0][*].address, 1)
+
 }
