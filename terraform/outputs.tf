6d27aebc3ac96bd30f8dff406b9e73b1b41d173b
JakUi <kawtan34@gmail.com>
2022-01-08 15:57:58 +0300

    Added LB-address output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,6 @@
 output "external_ip_address_app" {
   value = yandex_compute_instance.app.network_interface.0.nat_ip_address
 }
+output "external_load_balancer_address_app" {
+  value = yandex_lb_network_load_balancer.reddit_app_load_balancer.listener.*.external_address_spec[*][*]["address"]
+}
