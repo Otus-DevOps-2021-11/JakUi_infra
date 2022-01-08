a238c11b934bc5dc899ec9b7c49e73d0a704fdc8
JakUi <kawtan34@gmail.com>
2022-01-08 16:08:26 +0300

    Wrote template fo Loadbalancer

---
+++ terraform/lb.tf
@@ -0,0 +1,26 @@
+
+resource "yandex_lb_network_load_balancer" "reddit_app_load_balancer" {
+  name        = "reddit-app-load-balancer"
+
+  listener {
+    name = "listener-1"
+    port = 80
+    target_port = 9292
+    external_address_spec {
+      ip_version = "ipv4"
+    }
+  }
+
+  attached_target_group {
+    # target_group_id = "${yandex_lb_target_group.my-target-group.id}"
+    target_group_id = "enp92bdakjlqdr33mghf"
+
+    healthcheck {
+      name = "http"
+      http_options {
+        port = 9292
+        path = "/"
+      }
+    }
+  }
+}
