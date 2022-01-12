c9db88e1b4c29272f64a5e682f1b53f66cef4679
JakUi <kawtan34@gmail.com>
2022-01-12 15:45:05 +0300

    Commented unused code

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -14,9 +14,9 @@ output "internal_ip_app" {
 #   value = yandex_compute_instance.reddit-app.*.network_interface.0.ip_address[1]
 # }
 
-output "target_group_info" {
-  value = yandex_lb_target_group.target_group.id
-}
+# output "target_group_info" {
+#   value = yandex_lb_target_group.target_group.id
+# }
 
 # output "external_load_balancer_address_app" {
 #   value = element(yandex_lb_network_load_balancer.reddit_app_load_balancer.listener.*.external_address_spec[0][*].address, 1)
