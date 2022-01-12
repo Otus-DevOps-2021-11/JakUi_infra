86b555ea3fb3a5022f47880bcfdb46de548fbda6
JakUi <kawtan34@gmail.com>
2022-01-12 17:27:06 +0300

    Made homework before page 31

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,3 +5,17 @@ provider "yandex" {
   folder_id = var.folder_id
   zone      = var.zone
 }
+
+module "app" {
+  source          = "./modules/app"
+  public_key_path = var.public_key_path
+  app_disk_image  = var.app_disk_image
+  subnet_id       = var.subnet_id
+}
+
+module "db" {
+  source          = "./modules/db"
+  public_key_path = var.public_key_path
+  db_disk_image   = var.db_disk_image
+  subnet_id       = var.subnet_id
+}
