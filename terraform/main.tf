b4f310b8aaaf8e881456c328c366c44fbf6fcbbb
JakUi <kawtan34@gmail.com>
2022-01-12 15:45:35 +0300

    Changed IP address in resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,7 +22,7 @@ resource "yandex_compute_instance" "reddit-app" {
   }
 
   network_interface {
-    subnet_id = var.subnet_id
+    subnet_id = yandex_vpc_subnet.app-subnet.id
     nat       = true
   }
 
@@ -47,4 +47,15 @@ resource "yandex_compute_instance" "reddit-app" {
   provisioner "remote-exec" {
     script = "files/deploy.sh"
   }
+}
+
+resource "yandex_vpc_network" "app-network" {
+  name = "reddit-app-network"
+}
+
+resource "yandex_vpc_subnet" "app-subnet" {
+  name           = "reddit-app-subnet"
+  zone           = "ru-central1-a"
+  network_id     = "${yandex_vpc_network.app-network.id}"
+  v4_cidr_blocks = ["192.168.10.0/24"]
 }
