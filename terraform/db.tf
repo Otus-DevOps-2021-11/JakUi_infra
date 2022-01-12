ebdd5e0d2283b40271e5bf352e051785d379cb0a
JakUi <kawtan34@gmail.com>
2022-01-12 16:42:03 +0300

    Separate configs by files

---
+++ terraform/db.tf
@@ -0,0 +1,26 @@
+resource "yandex_compute_instance" "db" {
+  name = "reddit-db"
+  labels = {
+    tags = "reddit-db"
+  }
+
+  resources {
+    cores  = 1
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = var.db_disk_image
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.app-subnet.id
+    nat = true
+  }
+
+  metadata = {
+  ssh-keys = "ubuntu:${file(var.public_key_path)}"
+  }
+}
