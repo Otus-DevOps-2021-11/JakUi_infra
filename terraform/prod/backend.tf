39341bd60ea0cfe1546cc516ea8b5bd9c60c502f
JakUi <kawtan34@gmail.com>
2022-01-14 12:15:07 +0300

    Removed keys

--- terraform/prod/backend.tf
+++ terraform/prod/backend.tf
@@ -8,8 +8,6 @@ terraform {
     bucket     = "reddit-app-bucket"
     region     = "us-east-1"
     key        = "prod/terraform-state.tfstate"
-    access_key       = "GEW1m4aOadWoG90P74Rx"
-    secret_key       = "i0UQyDQdgqM4QllXSHVxQGNNQBpgmOpoxLPB_kI9"
     skip_region_validation      = true
     skip_credentials_validation = true
   }
