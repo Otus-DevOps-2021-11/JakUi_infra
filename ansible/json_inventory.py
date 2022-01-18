1faa09acf4d8a13911fb5f3d1afb43a1195c98a5
JakUi <kawtan34@gmail.com>
2022-01-18 18:57:11 +0300

    Code refactored

--- ansible/json_inventory.py
+++ ansible/json_inventory.py
@@ -6,56 +6,32 @@ def read_input_file():
     with open('inventory') as data_input:
         data = [l for l in data_input if l != "\n"]
 
-    groups_count = 0
-    for e in data:
-        if e.count("[") != 0:
-            groups_count += 1
-
-    return data, groups_count
-
-def make_template():
-    data, groups_count = read_input_file()
-    x = 1
-    level = json.dumps({f"GROUP_NAME_{x}": {"hosts": {"HOST_NAME": {"ansible_host": f"IP_{x}"}}}})
-    group = f"GROUP_NAME_{x}"
-    level_part = {"hosts": {"HOST_NAME": {"ansible_host": f"IP_{x}"}}}
-    inventory_template = f'"{group}":{level_part},' * groups_count
-    inventory_template = inventory_template.replace("'", '"')[: -1]
-    print(type(inventory_template))
-    print(inventory_template)
-
-
-    # inventory = json.dumps(inventory_template)
-
+    return data
 
 def make_json_inventory():
+
     inventory = dict()
     keys, elements = [], []
-    data, groups_count = read_input_file()
-    print(data)
+    data = read_input_file()
     x = 0
-    for e in range (0, len(data)):
-        if data[e].count("]") != 0:
-            group = data[e][1 : data[e].find("]")]
+    for d in range (0, len(data)):
+        if data[d].count("]") != 0:
+            group = data[d][1 : data[d].find("]")]
             inventory[group] = {"host": ''}
             keys.append(group)
             x += 1
-        elif data[e].count("]") == 0:
-            elements = data[e].split()
-            for s in range (0, len(elements)):
-                if elements[s].count("=") == 0:
-                    inventory[keys[x - 1]]["host"] = {elements[s] : ''}
+        elif data[d].count("]") == 0:
+            elements = data[d].split()
+            for e in range (0, len(elements)):
+                if elements[e].count("=") == 0:
+                    inventory[keys[x - 1]]["host"] = {elements[e] : ''}
                 else:
-                    # inventory[keys[x - 1]]["host"][elements[s][ : elements[s].find("=")]] = elements[s][elements[s].find("=") + 1 : ]
-                    inventory[keys[x - 1]]["host"][elements[s - 1]] = {elements[s][ : elements[s].find("=")] : elements[s][elements[s].find("=") + 1 : ]}
+                    inventory[keys[x - 1]]["host"][elements[e - 1]] = \
+                    {elements[e][ : elements[e].find("=")] : elements[e][elements[e].find("=") + 1 : ]}
 
     print(f"inventory {inventory}")
-    # print(keys)
-    # print(elements)
-    
-    # with open('inventory.json') as f:
-    #     f.write(json.loads(inventory))
 
-# read_input_file()
-# make_template()
+    with open('inventory.json', 'w') as f:
+        f.write(json.dumps(inventory))
+
 make_json_inventory()
