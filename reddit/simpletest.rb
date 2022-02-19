cb2508e3531553e0e7b7faca056aa17728f81913
JakUi <kawtan34@gmail.com>
2022-02-19 17:36:33 +0300

    Debug

--- reddit/simpletest.rb
+++ reddit/simpletest.rb
@@ -4,15 +4,15 @@ require 'rack/test'
 
 set :environment, :test
 
-class MyAppTest < Test::Unit::TestCase
-  include Rack::Test::Methods
+# class MyAppTest < Test::Unit::TestCase
+#   include Rack::Test::Methods
 
-  def app
-    Sinatra::Application
-  end
+#   def app
+#     Sinatra::Application
+#   end
 
-  def test_get_request
-    get '/'
-    assert last_response.ok?
-  end
-end
+#   def test_get_request
+#     get '/'
+#     assert last_response.ok?
+#   end
+# end
